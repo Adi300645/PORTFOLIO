d30c3ba4297741dbe63ca35e266ef12c4586ca04
adityayadav3005 <adityayadav300645@gmail.com>
2024-09-29 02:40:14 +0530

    Achivements

--- src/sections/Achivements/Achivements.js
+++ src/sections/Achivements/Achivements.js
@@ -9,14 +9,14 @@ import {
 } from "@/components/ui/carousel";
 import useOrientation from "@/customHooks/deviceInfo/useOrientation/useOrientation";
 import { Poppins } from "next/font/google";
+import Image from "next/image";
 
 const poppinsBold = Poppins({
     subsets: ["latin"],
     display: "swap",
     weight: ["600"],
-    adjustFontFallback: false
+    adjustFontFallback: false,
 });
-
 
 function Achivements() {
     const isPortrait = useOrientation();
@@ -29,9 +29,7 @@ function Achivements() {
                     >
                         Achivements
                     </div>
-                    <div
-                        className="h-56 w-full bg-cover shadow-xl mt-3 flex flex-col dark:rounded dark:border-2 bg-white dark:bg-transparent dark:border-[#777] overflow-hidden"
-                    >
+                    <div className="h-56 w-full bg-cover shadow-xl mt-3 flex flex-col dark:rounded dark:border-2 bg-white dark:bg-transparent dark:border-[#777] overflow-hidden">
                         <div
                             className="h-32 bg-cover"
                             style={{
@@ -44,8 +42,8 @@ function Achivements() {
                         </div>
                         <div className={`mt-2 mx-4 text-xs mb-3`}>
                             Ad velit id adipisicing pariatur sunt incididunt
-                            labore voluptate exercitation ipsum.
-                            labore voluptate exercitation ipsum.
+                            labore voluptate exercitation ipsum. labore
+                            voluptate exercitation ipsum.
                         </div>
                     </div>
                 </div>
@@ -54,12 +52,12 @@ function Achivements() {
 
     return (
         <div className="w-[92rem] flex flex-col relative">
-            <div
+            {/* <div
                 className={`w-[54rem] h-2 bg-[#444] rounded relative -left-6 mix-blend-hard-light ${styles.lineMoveX}`}
             ></div>
             <div
                 className={`h-[4rem] w-2 bg-[#444] rounded absolute left-5 mix-blend-hard-light  ${styles.lineMoveY}`}
-            ></div>
+            ></div> */}
             <div className="w-[92rem] flex justify-start mt-12">
                 <div className="relative left-20 bottom-6">
                     <div className="text-3xl font-semibold text-[#6C6C6C] inner_text_shadow">
@@ -88,25 +86,32 @@ function Achivements() {
                                                 bg-transparent"
                                 >
                                     <CarouselItem className="basis-1/4 group z-10 hover:border-0 dark:hover:border-[0.1px] transition-all duration-1000 hover:ml-96 dark:bg-transparent dark:border-[#aaa6] dark:backdrop-blur">
-                                        <div className="w-80 h-[28rem]  group-hover:w-[50rem] transition-all ease-in-out duration-1000 dark:bg-transparent ">
-                                            <div className="text-3xl font-semibold text-[#444] bg-gradient-to-r items-center justify-center flex h-full ">
-                                                Text
+                                        <div className="w-80 h-[28rem] flex items-center relative justify-center overflow-hidden group-hover:w-[50rem] transition-all ease-in-out duration-1000 dark:bg-transparent ">
+                                            <Image src={"/Showcase/TransOrgAnalyticsBG.jpeg"}  width={1500} height={1500} className="w-full h-full object-cover"/>
+                                            <div className="text font-semibold top-24 items-center justify-center flex h-full fixed flex-col text-black">
+                                                <div className="font-extrabold text-2xl">TransOrg Analytics</div>
+                                                <div className="text-xs w-64 transition-all group-hover:w-80 text-center font-bold mt-4">Achieved First Runner-Up in a Data Science Hackathon by analyzing loan eligibility using Real Data</div>
                                             </div>
                                         </div>
                                     </CarouselItem>
                                     <CarouselItem className="basis-1/4 group z-10 hover:border-0 dark:hover:border-[0.1px] transition-all duration-1000 dark:bg-transparent dark:border-[#aaa6] dark:backdrop-blur">
-                                        <div className="w-80 h-[28rem]  group-hover:w-[50rem] transition-all ease-in-out duration-1000 dark:bg-transparent ">
-                                            <div className="text-3xl font-semibold text-[#444] bg-gradient-to-r items-center justify-center flex h-full ">
-                                                Text
+                                        <div className="w-80 h-[28rem] flex items-center relative justify-center overflow-hidden group-hover:w-[50rem] transition-all ease-in-out duration-1000 dark:bg-transparent ">
+                                            <Image src={"/Showcase/LCBGHD_LV.jpg"}  width={1500} height={1500} className="w-full h-full object-cover"/>
+                                            <div className="text font-semibold top-8 items-center justify-center flex h-full fixed flex-col text-white">
+                                                <div className="font-extrabold text-2xl">LeetCode</div>
+                                                <div className="text-xs font-normal w-60 text-center mt-4">Among Top 24% of coders rating : 1590 and Solved over 450+ Questions</div>
                                             </div>
                                         </div>
                                     </CarouselItem>
                                     <CarouselItem className="basis-1/4 group z-10 hover:border-0 dark:hover:border-[0.1px] transition-all duration-1000 hover:mr-96 dark:bg-transparent dark:border-[#aaa6] dark:backdrop-blur">
-                                        <div className="w-80 h-[28rem]  group-hover:w-[50rem] transition-all ease-in-out duration-1000 dark:bg-transparent ">
-                                            <div className="text-3xl font-semibold text-[#444] bg-gradient-to-r items-center justify-center flex h-full ">
-                                                Text
+                                        <div className="w-80 h-[28rem] flex items-center relative justify-center overflow-hidden group-hover:w-[50rem] transition-all ease-in-out duration-1000 dark:bg-transparent ">
+                                            <Image src={"/Showcase/BuildATHON.jpg"}  width={1500} height={1500} className="w-full h-full object-cover"/>
+                                            <div className="text font-semibold bottom-10 items-center justify-center flex h-full fixed flex-col text-white">
+                                                <div className="font-extrabold text-2xl">Build-a-Thon</div>
+                                                <div className="text-xs font-normal w-60 text-center mt-4">Made An web 3 based Aplication to buy and sell Properties </div>
                                             </div>
                                         </div>
+                                        
                                     </CarouselItem>
                                 </CarouselContent>
                                 <CarouselPrevious />
@@ -130,6 +135,6 @@ function Achivements() {
             </div>
         </div>
     );
-}
+} 
 
 export default Achivements;
