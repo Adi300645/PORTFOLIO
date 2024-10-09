7d0b4ed934df600383094b15fff1c69efe487ae0
adityayadav3005 <adityayadav300645@gmail.com>
2024-10-09 11:44:58 +0530

    Depicting Changes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   env:{
     "EMAIL_USER":"anky3210@gmail.com",
-    "EMAIL_PASS":"amnk pmuw xcuw hsoz"
+    "EMAIL_PASS":"fmgf hrak jkfv opvr"
   }
 }
 
